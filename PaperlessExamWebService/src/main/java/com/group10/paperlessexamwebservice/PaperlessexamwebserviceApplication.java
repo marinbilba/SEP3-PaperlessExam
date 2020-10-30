fcb71c4e171eca9936cd1d8bb003e242613865e1
Marin <maryn777@gmail.com>
2020-10-30 10:20:08 +0100

    Java PaperlessExamWebService added(default template). Web service uses Spring Boot framework.

---
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/PaperlessexamwebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.group10.paperlessexamwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaperlessexamwebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaperlessexamwebserviceApplication.class, args);
+    }
+
+}
---
+++ PaperlessExamWebService/src/test/java/com/group10/paperlessexamwebservice/PaperlessexamwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group10.paperlessexamwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessexamwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
