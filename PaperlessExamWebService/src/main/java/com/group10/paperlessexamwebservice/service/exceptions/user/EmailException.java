c197d5ba2d54fcc5f439ca62fc89e12195bf68c7
Marin Bilba <maryn777@gmail.com>
2020-11-04 23:31:07 +0100

    Exception in case the email was or not found.

---
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/exceptions/user/EmailException.java
@@ -0,0 +1,8 @@
+package com.group10.paperlessexamwebservice.service.exceptions.user;
+
+public class EmailException extends Exception{
+    public EmailException(String message)
+    {
+        super(message);
+    }
+}
