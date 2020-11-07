faf2a54c8d2bbbfe607f8298318023384c9f64de
Marin Bilba <maryn777@gmail.com>
2020-11-07 17:13:50 +0100

    Interface which extends the JpaRepository and the custom interface which is used for database queries.

---
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repository/IUserRepository.java
@@ -0,0 +1,13 @@
+package com.group10.databaselayer.repository;
+
+
+import com.group10.databaselayer.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository that provides CRUD operations for user com.group10.databaselayer.entity
+ */
+@Repository
+public interface IUserRepository extends JpaRepository<User, Long>,ICustomUserRepository {
+}
