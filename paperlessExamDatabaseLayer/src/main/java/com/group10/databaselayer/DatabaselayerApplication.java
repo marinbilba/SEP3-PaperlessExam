91cdcecb42c87ab3fc62827d5572512a3d9c56a2
Marin Bilba <maryn777@gmail.com>
2020-11-07 12:24:27 +0100

    database tier teamplate

---
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/DatabaselayerApplication.java
@@ -0,0 +1,13 @@
+package com.group10.databaselayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaselayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaselayerApplication.class, args);
+    }
+
+}
---
+++ paperlessExamDatabaseLayer/src/test/java/com/group10/databaselayer/DatabaselayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group10.databaselayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaselayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
