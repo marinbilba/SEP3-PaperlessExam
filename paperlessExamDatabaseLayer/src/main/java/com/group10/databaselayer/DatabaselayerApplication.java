192768c470e96ca831e61089381508fb9f934d72
Marin Bilba <maryn777@gmail.com>
2020-11-07 17:12:01 +0100

    Excluded security configurations

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/DatabaselayerApplication.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/DatabaselayerApplication.java
@@ -2,8 +2,15 @@ package com.group10.databaselayer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+
 public class DatabaselayerApplication {
 
     public static void main(String[] args) {
