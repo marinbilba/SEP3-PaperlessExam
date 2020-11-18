4f325a88e2db5d620c1bc6b01366cb860ceaae79
Marin Bilba <maryn777@gmail.com>
2020-11-18 13:21:51 +0100

    Insignificant changes

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/controller/socketmediator/Server.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/controller/socketmediator/Server.java
@@ -26,6 +26,7 @@ public class Server {
     private UserController userController;
     @Autowired
     private RoleController roleController;
+
     private static final HashSet<Object> controllersSet = new HashSet<>();
 
     @PostConstruct
