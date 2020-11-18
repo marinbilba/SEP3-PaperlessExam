2976022fec8272007b9a28b1277e1b040347010e
Ioana Grigore <ioanaoctavia00@yahoo.com>
2020-11-18 13:42:05 +0100

    Whatever

--- SEP3.PaperlessExam/Data/PaperlessExamSevice/PaperlessExamServiceImpl.cs
+++ SEP3.PaperlessExam/Data/PaperlessExamSevice/PaperlessExamServiceImpl.cs
@@ -9,54 +9,67 @@ using SEP3.PaperlessExam.Pages.AdminView.ManageAccounts;
 
 namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
 {
-    public class PaperlessExamServiceImpl :IPaperlessExamService
+    public class PaperlessExamServiceImpl : IPaperlessExamService
     {
         private string uri = "http://localhost:8080";
         private readonly HttpClient client;
-        private User userDeserialize;
+
 
         public PaperlessExamServiceImpl()
         {
             client = new HttpClient();
         }
 
-        public  User LoginUser(User user)
+        public async Task<User> LoginUser(User user)
         {
-          string userSerialized = JsonSerializer.Serialize(user);
-             var content=new StringContent(userSerialized,Encoding.UTF8,"application/json");
-         
-             Task<HttpResponseMessage> responseMessage =
-                  client.PostAsync(uri+"/login", content);
-             string s =responseMessage.Result.Content.ReadAsStringAsync().Result;
-             if (responseMessage.IsCompletedSuccessfully)
-             {
-                 Console.WriteLine(s);
+            User userDeserialize = null;
+            HttpResponseMessage responseMessage;
+            string userSerialized = JsonSerializer.Serialize(user);
+            var content = new StringContent(userSerialized, Encoding.UTF8, "application/json");
+            // 1. Send POST request
+            try
+            {
+                responseMessage =
+                    await client.PostAsync(uri + "/login", content);
+                // 2. Check if the resource was found, else throw exception to the client
+                if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new Exception("Ooops, resource not found");
+                }
+            }
+            // 3. Catch the exception in case the Server is not running 
+            catch (HttpRequestException e)
+            {
+                throw new Exception("No connection could be made because the server is not responding");
+            }
 
-                 Console.WriteLine("yes");
+            string serverMessage = responseMessage.Content.ReadAsStringAsync().Result;
+            // 4. Check the response status codes, else throws the error message to the client
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // 5. Deserialize the object
+                string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
+                userDeserialize = JsonSerializer.Deserialize<User>(readAsStringAsync);
+                Console.WriteLine(userDeserialize.Role.Name);
+            }
 
-             }
-             else
-             {
-                 Console.WriteLine(responseMessage.Result.StatusCode);
-                 Console.WriteLine("no");
-             }
-             User userDeserialize = JsonSerializer.Deserialize<User>(s);
+            else if (responseMessage.StatusCode == HttpStatusCode.Forbidden)
+            {
+                Console.WriteLine();
 
+                throw new Exception(serverMessage);
+            }
 
-
-
-       //here the user object should be deserialized
-
-        return new User("1111", "2222", "3333", "4444", "5555", new Role(2, "Student"));
+            return userDeserialize;
         }
+
         
         public async Task CreateUserAsync(User user)
         {
-            string accountSerialized = JsonSerializer.Serialize(user);
-            StringContent content = new StringContent(accountSerialized, Encoding.UTF8, "application/json");
-            HttpResponseMessage responseMessage = await client.PostAsync(uri + "/createUser", content);
-            Console.WriteLine(responseMessage.ToString());
+            // string accountSerialized = JsonSerializer.Serialize(user);
+            // StringContent content = new StringContent(accountSerialized, Encoding.UTF8, "application/json");
+            // HttpResponseMessage responseMessage = await client.PostAsync(uri + "/createUser", content);
+            // Console.WriteLine(responseMessage.ToString());
         }
     }
- 
 }
