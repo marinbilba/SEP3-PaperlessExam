440748c237289870b7c65dc3d40a1a1b6f17ed29
Marin Bilba <maryn777@gmail.com>
2020-11-18 13:24:55 +0100

    Added Exception in case the connection can not be made

---
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/exceptions/other/ServiceNotAvailable.java
@@ -0,0 +1,8 @@
+package com.group10.paperlessexamwebservice.service.exceptions.other;
+
+public class ServiceNotAvailable  extends Exception{
+    public ServiceNotAvailable(String message)
+    {
+        super(message);
+    }
+}
