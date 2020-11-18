84c01fe9d6b54f3f8baa084dc5cc666732346fbe
Marin Bilba <maryn777@gmail.com>
2020-11-18 13:21:16 +0100

    Modified the loginuser method and added the Exception throw to method signature

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/PaperlessExamServiceImpl.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/PaperlessExamServiceImpl.java
@@ -3,12 +3,14 @@ package com.group10.paperlessexamwebservice.service;
 import com.group10.paperlessexamwebservice.databaserequests.IUserRequests;
 import com.group10.paperlessexamwebservice.model.Role;
 import com.group10.paperlessexamwebservice.model.User;
+import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
 import com.group10.paperlessexamwebservice.service.exceptions.user.PasswordNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,13 +23,13 @@ public class PaperlessExamServiceImpl implements IUserService {
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public User logInUser(User user) throws UsernameNotFoundException, PasswordNotFoundException {
-//        Check user by username
-        if (!userRequest.usernameExist(user.getUsername())) {
+    public User logInUser(User user) throws ServiceNotAvailable,PasswordNotFoundException {
+        //        Get the user object by username for further validation
+        User requestedUserFromTheDatabase = userRequest.getUserByUsername(user.getUsername());
+        //        Check user by username
+        if (userRequest.getUserByUsername(user.getUsername()) == null) {
             throw new UsernameNotFoundException("Username is incorrect");
         }
-//        Get the user object by username for further validation
-        User requestedUserFromTheDatabase = userRequest.getUserByUsername(user.getUsername());
         // Password validation
         if (!requestedUserFromTheDatabase.getPassword().equals(user.getPassword())) {
             throw new PasswordNotFoundException("Password is incorrect");
@@ -63,7 +65,7 @@ public class PaperlessExamServiceImpl implements IUserService {
         Role tempRole = userRequest.getRoleIdByName(user.getRole().getName());
         // set the recieved role to the current user
         user.setRole(tempRole);
-        if (userRequest.usernameExist(user.getUsername())) {
+        if (userRequest.getUserByUsername(user.getUsername())==null) {
             throw new Exception("Username");
             // throw la exceptie !!!!!
         } else {
