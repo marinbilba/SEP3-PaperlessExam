6548b2eb2b830794f1b8666e59c6970ccd0cf5b5
Marin Bilba <maryn777@gmail.com>
2020-11-18 13:18:43 +0100

    Added exception to method signature

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/IUserService.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/IUserService.java
@@ -1,15 +1,17 @@
 package com.group10.paperlessexamwebservice.service;
 
 import com.group10.paperlessexamwebservice.model.User;
+import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
 import com.group10.paperlessexamwebservice.service.exceptions.user.DataBaseException;
 import com.group10.paperlessexamwebservice.service.exceptions.user.EmailException;
 import com.group10.paperlessexamwebservice.service.exceptions.user.PasswordNotFoundException;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface IUserService {
 
-    User logInUser(User user) throws PasswordNotFoundException;
+    User logInUser(User user) throws ServiceNotAvailable, PasswordNotFoundException;
     User createUser(User user) throws Exception;
 
 //    Returns the list of all stored users
