f7d5568b5c48e58acc64d28038e096cf89433f84
Marin Bilba <maryn777@gmail.com>
2020-11-21 10:57:35 +0100

    THREAD THROTTLING BUG FIX

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/DatabaselayerApplication.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/DatabaselayerApplication.java
@@ -3,10 +3,11 @@ package com.group10.databaselayer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-
+@EnableJpaRepositories
 public class DatabaselayerApplication {
 
     public static void main(String[] args)  {
