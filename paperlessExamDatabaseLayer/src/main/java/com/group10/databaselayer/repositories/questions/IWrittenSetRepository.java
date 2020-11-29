0de2f0b6d79dfd4acf28b360ff3e895deeeb659a
Marin Bilba <maryn777@gmail.com>
2020-11-29 20:33:36 +0100

    Added repository for WrittenSet

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IWrittenSetRepository.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IWrittenSetRepository.java
@@ -1,10 +1,14 @@
 package com.group10.databaselayer.repositories.questions;
 
+import com.group10.databaselayer.entity.questions.QuestionsSet;
 import com.group10.databaselayer.entity.questions.written.WrittenSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface IWrittenSetRepository extends JpaRepository<WrittenSet,String> {
+import javax.transaction.Transactional;
 
-}
+@Repository
+public interface IWrittenSetRepository extends JpaRepository<WrittenSet,QuestionsSet> {
+ }
+
+
