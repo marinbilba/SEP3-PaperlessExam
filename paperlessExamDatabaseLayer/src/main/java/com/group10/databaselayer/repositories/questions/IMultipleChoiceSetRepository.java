57dcc910610722651b4d220cf0633442e2a23b5f
Marin Bilba <maryn777@gmail.com>
2020-11-29 20:33:21 +0100

    Added repository for MultipleChoiceSet

---
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IMultipleChoiceSetRepository.java
@@ -0,0 +1,11 @@
+package com.group10.databaselayer.repositories.questions;
+
+import com.group10.databaselayer.entity.questions.QuestionsSet;
+import com.group10.databaselayer.entity.questions.multiplechoice.MultipleChoiceSet;
+import com.group10.databaselayer.entity.questions.written.WrittenSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMultipleChoiceSetRepository extends JpaRepository<MultipleChoiceSet, QuestionsSet> {
+}
