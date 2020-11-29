c17fcc76f1ed4ea3edc1f16f90aa7b2893495b5f
Marin Bilba <maryn777@gmail.com>
2020-11-29 23:01:45 +0100

    Added repository documentation

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IMultipleChoiceSetRepository.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IMultipleChoiceSetRepository.java
@@ -6,6 +6,9 @@ import com.group10.databaselayer.entity.questions.written.WrittenSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository that provides CRUD operations for MultipleChoiceSet entity{@link MultipleChoiceSet}
+ */
 @Repository
 public interface IMultipleChoiceSetRepository extends JpaRepository<MultipleChoiceSet, QuestionsSet> {
 }
--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IWrittenSetRepository.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/IWrittenSetRepository.java
@@ -2,11 +2,16 @@ package com.group10.databaselayer.repositories.questions;
 
 import com.group10.databaselayer.entity.questions.QuestionsSet;
 import com.group10.databaselayer.entity.questions.written.WrittenSet;
+import com.group10.databaselayer.entity.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+
+/**
+ * Repository that provides CRUD operations for WrittenSet entity{@link WrittenSet}
+ */
 @Repository
 public interface IWrittenSetRepository extends JpaRepository<WrittenSet,QuestionsSet> {
  }
