96eefcb624397f9e89546fe694ad45b0a027b5b1
Marin Bilba <maryn777@gmail.com>
2020-11-30 16:22:33 +0100

    Added custom exception

---
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/exception/questions/QuestionNotFound.java
@@ -0,0 +1,15 @@
+package com.group10.databaselayer.exception.questions;
+
+/**
+ * Exception Question not found.
+ */
+public class QuestionNotFound extends Exception{
+    /**
+     * Instantiates a new Question not found object with the error message.
+     *
+     * @param message the message
+     */
+    public QuestionNotFound(String message){
+        super(message);
+    }
+}
