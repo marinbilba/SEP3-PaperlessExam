39df9c3c8aecb79a0d3cfd5d1c239c22d3a1275b
Marin Bilba <maryn777@gmail.com>
2020-12-01 16:56:16 +0100

    Added method findByMultipleChoiceSet

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/multiplechoice/IMultipleChoiceQuestionRepository.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/repositories/questions/multiplechoice/IMultipleChoiceQuestionRepository.java
@@ -2,8 +2,12 @@ package com.group10.databaselayer.repositories.questions.multiplechoice;
 
 import com.group10.databaselayer.entity.questions.Question;
 import com.group10.databaselayer.entity.questions.multiplechoice.MultipleChoiceQuestion;
+import com.group10.databaselayer.entity.questions.multiplechoice.MultipleChoiceSet;
 import com.group10.databaselayer.entity.questions.written.WrittenQuestion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IMultipleChoiceQuestionRepository extends JpaRepository<MultipleChoiceQuestion, Question> {
+    List<MultipleChoiceQuestion> findByMultipleChoiceSet(MultipleChoiceSet multipleChoiceSet);
 }
