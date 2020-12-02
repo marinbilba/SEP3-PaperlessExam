1e98800908fadc9885b8182af4766e8772b93c96
Marin Bilba <maryn777@gmail.com>
2020-12-02 10:51:04 +0100

    Added throw EmailException to createUser method

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/IUserService.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/IUserService.java
@@ -2,17 +2,14 @@ package com.group10.paperlessexamwebservice.service;
 
 import com.group10.paperlessexamwebservice.model.User;
 import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
-import com.group10.paperlessexamwebservice.service.exceptions.user.PasswordException;
-import com.group10.paperlessexamwebservice.service.exceptions.user.UsernameFoundException;
-import com.group10.paperlessexamwebservice.service.exceptions.user.UserNotFound;
-import com.group10.paperlessexamwebservice.service.exceptions.user.UsernameNotMatchEmail;
+import com.group10.paperlessexamwebservice.service.exceptions.user.*;
 
 import java.util.List;
 
 public interface IUserService {
 
     User logInUser(User user) throws ServiceNotAvailable, PasswordException;
-    User createUser(User user) throws UsernameNotMatchEmail, PasswordException, ServiceNotAvailable, UsernameFoundException;
+    User createUser(User user) throws UsernameNotMatchEmail, PasswordException, ServiceNotAvailable, UsernameFoundException, EmailException;
 
 //    Returns the list of all stored users
     List<User> getAllUsersList();
