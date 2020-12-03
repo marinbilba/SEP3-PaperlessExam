a2da958e2496c47a51cf370881fa70868198ae2a
Marin Bilba <maryn777@gmail.com>
2020-12-03 19:41:42 +0100

    No changes

--- paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/controller/networkcontainer/RequestOperation.java
+++ paperlessExamDatabaseLayer/src/main/java/com/group10/databaselayer/controller/networkcontainer/RequestOperation.java
@@ -12,9 +12,9 @@ import javax.persistence.Enumerated;
  */
 
 public enum RequestOperation {
-      //    User request
-      @Enumerated
-      GET_USER_BY_USERNAME,
+    //    User request
+    @Enumerated
+    GET_USER_BY_USERNAME,
     CREATE_USER,
     GET_ROLE_ID_BY_NAME,
     GET_USERS_BY_FIRST_NAME,
