f7a20e04725dfd959cc95aa7ccb8397fe5fc079b
Ioana Grigore <ioanaoctavia00@yahoo.com>
2020-12-04 00:40:09 +0100

    Request for delete and update user + corrected User class

--- SEP3.PaperlessExam/Data/PaperlessExamSevice/PaperlessExamServiceImpl.cs
+++ SEP3.PaperlessExam/Data/PaperlessExamSevice/PaperlessExamServiceImpl.cs
@@ -114,7 +114,7 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
 
         public async Task<IList<User>> FindByFirstName(string name)
         {
-
+            
             IList<User> usersDeserialize = null;
             HttpResponseMessage responseMessage;
             IList<string> usersSerialized = new[] {JsonSerializer.Serialize(name)};
@@ -162,7 +162,7 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
             }
             return usersDeserialize;
             // List<User> users = new List<User>();
-            // users.Add(new User("HEJ", "tyuiop", "fgjukil", "dfgnhjmk", "tgyhuj", new Role("teacher")));
+            // users.Add(new User("HEJ", "tyuiop", "fgjukil", "dfgnhjmk", "tgyhuj", new Role("teacher"), ""));
             // return users;
         }
          
@@ -206,13 +206,87 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
                 throw new Exception(serverMessage);
             }
            
-
+            // User user = new User("HEJ", "tyuiop", "fgjukil", "dfgnhjmk", "tgyhuj", new Role("teacher"),"@");
             return userDeserialize;
+            // return user;
         }
 
-        public async Task<string> DeleteUser(string username)
+        public async Task DeleteUser(string username)
         {
-            return "a";
+            User userDeserialize = null;
+            HttpResponseMessage responseMessage;
+            string userSerialized = JsonSerializer.Serialize(username);
+            var content = new StringContent(userSerialized, Encoding.UTF8, "application/json");
+            // 1. Send POST request
+            try
+            {
+                responseMessage =
+                    await client.PostAsync(uri + "/deleteUser", content);
+                // 2. Check if the resource was found, else throw exception to the client
+                if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new Exception("Ooops, resource not found");
+                }
+            }
+            // 3. Catch the exception in case the Server is not running 
+            catch (HttpRequestException e)
+            {
+                throw new Exception("No connection could be made because the server is not responding");
+            }
+
+            string serverMessage = responseMessage.Content.ReadAsStringAsync().Result;
+            // 4. Check the response status codes, else throws the error message to the client
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // 5. Deserialize the object
+                string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
+                userDeserialize = JsonSerializer.Deserialize<User>(readAsStringAsync);
+                Console.WriteLine(userDeserialize.Role.Name);
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new Exception(serverMessage);
+            }  
+
+        }
+
+        public async Task UpdateUser(User user)
+        {
+            User userDeserialize = null;
+            HttpResponseMessage responseMessage;
+            string userSerialized = JsonSerializer.Serialize(user);
+            var content = new StringContent(userSerialized, Encoding.UTF8, "application/json");
+            // 1. Send POST request
+            try
+            {
+                responseMessage =
+                    await client.PostAsync(uri + "/updateUser", content);
+                // 2. Check if the resource was found, else throw exception to the client
+                if (responseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new Exception("Ooops, resource not found");
+                }
+            }
+            // 3. Catch the exception in case the Server is not running 
+            catch (HttpRequestException e)
+            {
+                throw new Exception("No connection could be made because the server is not responding");
+            }
+
+            string serverMessage = responseMessage.Content.ReadAsStringAsync().Result;
+            // 4. Check the response status codes, else throws the error message to the client
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // 5. Deserialize the object
+                string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
+                userDeserialize = JsonSerializer.Deserialize<User>(readAsStringAsync);
+                Console.WriteLine(userDeserialize.Role.Name);
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new Exception(serverMessage);
+            }   
+           
         }
     }
 
