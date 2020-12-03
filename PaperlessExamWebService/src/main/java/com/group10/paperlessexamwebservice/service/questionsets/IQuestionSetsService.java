d30ff28b437890ca76b9f38ef27ec651b8a5f910
Marin Bilba <maryn777@gmail.com>
2020-12-03 21:10:16 +0100

    Added method createMultipleChoiceSet

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/questionsets/IQuestionSetsService.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/questionsets/IQuestionSetsService.java
@@ -3,10 +3,13 @@ package com.group10.paperlessexamwebservice.service.questionsets;
 import com.group10.paperlessexamwebservice.model.questions.multiplechoice.MultipleChoiceQuestion;
 import com.group10.paperlessexamwebservice.model.questions.multiplechoice.MultipleChoiceSet;
 import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
+import com.group10.paperlessexamwebservice.service.exceptions.other.UnexpectedError;
 import com.group10.paperlessexamwebservice.service.exceptions.questionsets.*;
 
 public interface IQuestionSetsService {
     boolean validateMultipleChoiceSet(MultipleChoiceSet multipleChoiceSet) throws EmptyQuestionSet, EmptyQuestionSetTitleOrTopic, QuestionSetAlreadyExists, ServiceNotAvailable;
 
     boolean validateMultipleChoiceSetQuestion(MultipleChoiceQuestion multipleChoiceQuestion) throws EmptyQuestionSetQuestions, MultipleChoiceQuestionOptionError;
+
+    MultipleChoiceSet createMultipleChoiceSet(MultipleChoiceSet multipleChoiceSet) throws ServiceNotAvailable, UnexpectedError;
 }
