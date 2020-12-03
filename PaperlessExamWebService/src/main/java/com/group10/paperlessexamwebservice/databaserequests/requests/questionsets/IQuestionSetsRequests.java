7250cdcbe9018cec19e1a24c799a0a92d2993547
Marin Bilba <maryn777@gmail.com>
2020-12-03 19:36:27 +0100

    Add  interfaces for question sets requests.

---
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/databaserequests/requests/questionsets/IQuestionSetsRequests.java
@@ -0,0 +1,9 @@
+package com.group10.paperlessexamwebservice.databaserequests.requests.questionsets;
+
+import com.group10.paperlessexamwebservice.model.questions.multiplechoice.MultipleChoiceSet;
+import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
+
+public interface IQuestionSetsRequests {
+    boolean existsMultipleChoiceSet(MultipleChoiceSet multipleChoiceSet) throws ServiceNotAvailable;
+
+}
