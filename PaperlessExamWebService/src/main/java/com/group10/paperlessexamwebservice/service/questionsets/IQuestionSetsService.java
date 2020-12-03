e738bd3253cdf6839d7d037935fdf1f29e7cb5e8
Marin Bilba <maryn777@gmail.com>
2020-12-03 19:37:23 +0100

    Add validateMultipleChoiceSet and validateMultipleChoiceSetQuestion methods

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/questionsets/IQuestionSetsService.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/questionsets/IQuestionSetsService.java
@@ -1,4 +1,12 @@
 package com.group10.paperlessexamwebservice.service.questionsets;
 
+import com.group10.paperlessexamwebservice.model.questions.multiplechoice.MultipleChoiceQuestion;
+import com.group10.paperlessexamwebservice.model.questions.multiplechoice.MultipleChoiceSet;
+import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
+import com.group10.paperlessexamwebservice.service.exceptions.questionsets.*;
+
 public interface IQuestionSetsService {
+    boolean validateMultipleChoiceSet(MultipleChoiceSet multipleChoiceSet) throws EmptyQuestionSet, EmptyQuestionSetTitleOrTopic, QuestionSetAlreadyExists, ServiceNotAvailable;
+
+    boolean validateMultipleChoiceSetQuestion(MultipleChoiceQuestion multipleChoiceQuestion) throws EmptyQuestionSetQuestions, MultipleChoiceQuestionOptionError;
 }
