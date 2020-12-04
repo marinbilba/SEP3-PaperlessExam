1526fac7285e8281aa43ad968b0ef5a60fc4605d
Marin Bilba <maryn777@gmail.com>
2020-12-04 11:02:01 +0100

    Added method to updateUser

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/user/IUserService.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/user/IUserService.java
@@ -2,13 +2,15 @@ package com.group10.paperlessexamwebservice.service.user;
 
 import com.group10.paperlessexamwebservice.model.user.User;
 import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
+import com.group10.paperlessexamwebservice.service.exceptions.other.UnexpectedError;
 import com.group10.paperlessexamwebservice.service.exceptions.user.*;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.List;
 
 public interface IUserService {
 
-    User logInUser(User user) throws ServiceNotAvailable, PasswordException;
+    User logInUser(User user) throws ServiceNotAvailable, PasswordException, com.group10.paperlessexamwebservice.service.exceptions.user.UsernameNotFoundException;
     User createUser(User user) throws UsernameNotMatchEmail, PasswordException, ServiceNotAvailable, UsernameFoundException, EmailException;
 
 //    Returns the list of all stored users
@@ -17,4 +19,7 @@ public interface IUserService {
     List<User> getUsersByFirstName(String firstName) throws ServiceNotAvailable, UserNotFound;
 
   User getUsersByUsername(String username) throws ServiceNotAvailable, UserNotFound;
+
+    User updateUser(User user) throws UsernameNotFoundException, NullFieldUser, ServiceNotAvailable, UnexpectedError, EmailException, UsernameNotMatchEmail, PasswordException, com.group10.paperlessexamwebservice.service.exceptions.user.UsernameNotFoundException;
+
 }
