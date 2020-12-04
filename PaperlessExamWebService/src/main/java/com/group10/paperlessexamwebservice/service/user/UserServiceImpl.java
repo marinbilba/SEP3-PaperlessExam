f2eb6d273755758fec74e82dc025c08b844a849c
Marin Bilba <maryn777@gmail.com>
2020-12-04 11:05:05 +0100

    Added updateUser method and its validation

--- PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/user/UserServiceImpl.java
+++ PaperlessExamWebService/src/main/java/com/group10/paperlessexamwebservice/service/user/UserServiceImpl.java
@@ -4,14 +4,17 @@ import com.group10.paperlessexamwebservice.databaserequests.requests.user.IUserR
 import com.group10.paperlessexamwebservice.model.user.Role;
 import com.group10.paperlessexamwebservice.model.user.User;
 import com.group10.paperlessexamwebservice.service.exceptions.other.ServiceNotAvailable;
+import com.group10.paperlessexamwebservice.service.exceptions.other.UnexpectedError;
 import com.group10.paperlessexamwebservice.service.exceptions.user.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * The type User service.
+ */
 @Service
 public class UserServiceImpl implements IUserService {
 
@@ -25,14 +28,15 @@ public class UserServiceImpl implements IUserService {
      * Send a request that returns a User object to {@link IUserRequests}. The requested user object
      * is checked. If the user is null => the user was not found in the database. The password received from
      * the database is compared with the one provided by the client.
+     *
      * @param user user object
      * @return If the credentials are validated successfully the received User object from the database will be
      * returned to the Client.
      * @throws ServiceNotAvailable if there are connection problems with the third tier
-     * @throws PasswordException if credentials are incorrect.
+     * @throws PasswordException   if credentials are incorrect.
      */
     @Override
-    public User logInUser(User user) throws ServiceNotAvailable, PasswordException {
+    public User logInUser(User user) throws ServiceNotAvailable, PasswordException, UsernameNotFoundException {
         //        Get the user object by username for further validation
         User requestedUserFromTheDatabase = userRequest.getUserByUsername(user.getUsername());
         //        Check user by username
@@ -48,18 +52,18 @@ public class UserServiceImpl implements IUserService {
 
     /**
      * Creates and stores a user in the database. The following steps are performed
-     *  1. Check if the username exists in the database, else exception is thrown
-     *  2. Check if the username does match the substring of the email until the '@' char, else exception is thrown
-     *  3. Password validation
-     *  4. Get the role id by provided name from the Client
-     *  5. Set the received role id to the current user object instance
-     *  6. Create the user
-     *  
+     * 1. Check if the username exists in the database, else exception is thrown
+     * 2. Check if the username does match the substring of the email until the '@' char, else exception is thrown
+     * 3. Password validation
+     * 4. Get the role id by provided name from the Client
+     * 5. Set the received role id to the current user object instance
+     * 6. Create the user
+     *
      * @param user user object that should be stored
      * @return created User object
-     * @throws UsernameNotMatchEmail the username does not match the substring of the email until the '@' char
-     * @throws PasswordException if credentials are incorrect
-     * @throws ServiceNotAvailable if there are connection problems with the third tier
+     * @throws UsernameNotMatchEmail  the username does not match the substring of the email until the '@' char
+     * @throws PasswordException      if credentials are incorrect
+     * @throws ServiceNotAvailable    if there are connection problems with the third tier
      * @throws UsernameFoundException the username already exists in the database
      */
     @Override
@@ -70,30 +74,32 @@ public class UserServiceImpl implements IUserService {
             throw new UsernameFoundException("Username already exists");
         }
         // Check username == email before@
-       String subtractedString= getEmailSubstring(user.getEmail());
-        if(!user.getUsername().equals(subtractedString)){
+        String subtractedString = getEmailSubstring(user.getEmail());
+        if (!user.getUsername().equals(subtractedString)) {
             throw new UsernameNotMatchEmail("Username must match the email until the '@' sign ");
         }
         //   Password validation
-        if(!user.getPassword().equals(user.getConfirmPassword())){
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
             throw new PasswordException("Password does not match Confirm Password field");
         }
         // Get role id
-        Role roleId= userRequest.getRoleIdByName(user.getRole().getName());
+        Role roleId = userRequest.getRoleIdByName(user.getRole().getName());
         user.setRole(roleId);
-        return userRequest.createUser(user);
+        return userRequest.createUpdateUser(user);
     }
+
     /**
      * Method to subtract the String until the '@' character
+     *
      * @param email email to check
      * @return the String until the '@' character
      */
     private String getEmailSubstring(String email) throws EmailException {
-        if(email.contains("@")){
-            int i =email.indexOf("@");
-            return email.substring(0,i);
+        if (email.contains("@")) {
+            int i = email.indexOf("@");
+            return email.substring(0, i);
         }
-       throw new EmailException("Email must contain character '@'");
+        throw new EmailException("Email must contain character '@'");
     }
 
     @Override
@@ -103,8 +109,8 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public List<User> getUsersByFirstName(String firstName) throws ServiceNotAvailable, UserNotFound {
-        List<User> usersList= userRequest.getUsersByFirstName(firstName);
-        if(!usersList.isEmpty()){
+        List<User> usersList = userRequest.getUsersByFirstName(firstName);
+        if (!usersList.isEmpty()) {
             return usersList;
         }
         throw new UserNotFound("Users by given first name were not found");
@@ -112,11 +118,67 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public User getUsersByUsername(String username) throws ServiceNotAvailable, UserNotFound {
-        User user=userRequest.getUserByUsername(username);
-        if(user!=null){
+        User user = userRequest.getUserByUsername(username);
+        if (user != null) {
             return user;
+        } else throw new UserNotFound("Username was not found in the system");
+    }
+
+    @Override
+    //  todo, check if current user.equals with the one from the database
+    public User updateUser(User user) throws NullFieldUser, ServiceNotAvailable, UnexpectedError, EmailException, UsernameNotMatchEmail, PasswordException, UsernameNotFoundException {
+        boolean validatedSuccessful = checkNullUserFields(user);
+        if (validatedSuccessful) {
+            User fetchedUser = userRequest.getUserByUsername(user.getUsername());
+            if (fetchedUser == null) {
+                throw new UsernameNotFoundException("Username was not found in the system");
+            }
+            String subtractedString = getEmailSubstring(user.getEmail());
+            if (!user.getUsername().equals(subtractedString)) {
+                throw new UsernameNotMatchEmail("Username must match the email until the '@' sign ");
+            } else if (!user.getPassword().equals(user.getConfirmPassword())) {
+                throw new PasswordException("Password does not match Confirm Password field");
+            }
+            Long userID = fetchedUser.getId();
+            fetchedUser = user;
+
+            Role fetchedRole = userRequest.getRoleIdByName(user.getRole().getName());
+//            Set id and role fetched from the db
+            fetchedUser.setId(userID);
+            fetchedUser.setRole(fetchedRole);
+            userRequest.createUpdateUser(fetchedUser);
+            return fetchedUser;
         }
-        else throw new UserNotFound("Username was not found in the system");
+        throw new UnexpectedError("Something went wrong");
+
+    }
+
+    /**
+     * Method check if user fields are not null, except id.
+     *
+     * @param user
+     * @return
+     * @throws NullFieldUser if one of the fields is null
+     */
+    private boolean checkNullUserFields(User user) throws NullFieldUser {
+        if (user.getUsername() == null) {
+            throw new NullFieldUser("Username must be entered");
+        } else if (user.getEmail() == null) {
+            throw new NullFieldUser("Email must be entered");
+        } else if (user.getFirstName() == null) {
+            throw new NullFieldUser("First name must be entered");
+
+        } else if (user.getLastName() == null) {
+            throw new NullFieldUser("Last name must be entered");
+
+        } else if (user.getRole() == null) {
+            throw new NullFieldUser("Role must be set");
+        } else if (user.getPassword() == null) {
+            throw new NullFieldUser("Password  must be entered");
+        } else if (user.getConfirmPassword() == null) {
+            throw new NullFieldUser("Confirm Password  must be entered");
+        }
+        return true;
     }
 
 }
