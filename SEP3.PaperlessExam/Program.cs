ad2570db726ea75c32832e36f4d933046f5fd862
Marin Bilba <maryn777@gmail.com>
2020-12-05 21:42:34 +0100

    No changes

--- SEP3.PaperlessExam/Program.cs
+++ SEP3.PaperlessExam/Program.cs
@@ -16,10 +16,12 @@ namespace SEP3.PaperlessExam
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        
     }
 }
