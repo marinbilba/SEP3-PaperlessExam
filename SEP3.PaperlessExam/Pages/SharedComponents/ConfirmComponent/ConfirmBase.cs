67fd92703aadcb45b30601eb9b05dd725549cf3a
Marin Bilba <maryn777@gmail.com>
2020-12-05 21:36:55 +0100

    Added confirmation popup base with EventCallback

---
+++ SEP3.PaperlessExam/Pages/SharedComponents/ConfirmComponent/ConfirmBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace SEP3.PaperlessExam.Pages.SharedComponents.ConfirmComponent
+{
+    public class ConfirmBase : ComponentBase
+    {
+        [Parameter] public string ConfirmationTitle { get; set; } = "Delete Confirmation";
+        [Parameter] public string ConfirmationMessage { get; set; } = "Are you sure you want to delete?";
+        protected bool ShowConfirmation { get; set; }
+
+        public void Show()
+        {
+            ShowConfirmation = true;
+            StateHasChanged();
+        }
+        
+        protected async Task OnConfirmationChange(bool value)
+        {
+            ShowConfirmation = false;
+            await ConfirmationChanged.InvokeAsync(value);
+        }
+
+        [Parameter] public EventCallback<bool> ConfirmationChanged { get; set; }
+    }
+}
