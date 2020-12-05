681a9825fd3b2357a63e2679c47b38851b566106
Marin Bilba <maryn777@gmail.com>
2020-12-05 21:45:21 +0100

    Fixed all request methods

--- SEP3.PaperlessExam/Data/PaperlessExamSevice/UserServiceImpl.cs
+++ SEP3.PaperlessExam/Data/PaperlessExamSevice/UserServiceImpl.cs
@@ -1,6 +1,6 @@
-﻿﻿using System;
- using System.Collections.Generic;
- using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -10,16 +10,17 @@ using SEP3.PaperlessExam.Pages.AdminView.ManageAccounts;
 
 namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
 {
-    public class PaperlessExamServiceImpl : IPaperlessExamService
+    public class UserServiceImpl : IUserService
     {
         private string uri = "http://localhost:8080";
         private readonly HttpClient client;
 
 
-        public PaperlessExamServiceImpl()
+        public UserServiceImpl()
         {
             client = new HttpClient();
         }
+
         public async Task<User> LoginUser(User user)
         {
             User userDeserialize = null;
@@ -58,14 +59,16 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
                 Console.WriteLine();
 
                 throw new Exception(serverMessage);
-            }else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
             {
                 throw new Exception(serverMessage);
             }
 
             return userDeserialize;
         }
-        public async Task CreateUserAsync(User user)
+
+        public async Task<User> CreateUserAsync(User user)
         {
             User userDeserialize = null;
             HttpResponseMessage responseMessage;
@@ -100,7 +103,8 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
             else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
             {
                 throw new Exception(serverMessage);
-            }   else if (responseMessage.StatusCode == HttpStatusCode.Unauthorized)
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.Unauthorized)
             {
                 throw new Exception(serverMessage);
             }
@@ -109,74 +113,62 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
                 throw new Exception(serverMessage);
             }
 
-            
+            return userDeserialize;
         }
 
-        public async Task<IList<User>> FindByFirstName(string name)
+        public async Task<IList<User>> FindByFirstName(string firstName)
         {
-            
             IList<User> usersDeserialize = null;
             HttpResponseMessage responseMessage;
-            IList<string> usersSerialized = new[] {JsonSerializer.Serialize(name)};
-            foreach (var s in usersSerialized)
+          // 1. Send GET request
+            try
             {
-                var content = new StringContent(s, Encoding.UTF8, "application/json");
-                // 1. Send POST request
-                try
+                responseMessage =
+                    await client.GetAsync($"{uri}/user/getUsersByFirstName/{firstName}");
+                // 2. Check if the resource was found, else throw exception to the client
+                if (responseMessage.StatusCode == HttpStatusCode.NotFound)
                 {
-                    responseMessage =
-                        await client.PostAsync(uri + "/user/getUserByFirstName", content);
-                    // 2. Check if the resource was found, else throw exception to the client
-                    if (responseMessage.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        throw new Exception("Ooops, resource not found");
-                    }
+                    throw new Exception("Ooops, resource not found");
                 }
-                // 3. Catch the exception in case the Server is not running 
-                catch (HttpRequestException e)
-                {
-                    throw new Exception("No connection could be made because the server is not responding");
-                }
-            
-                string serverMessage = responseMessage.Content.ReadAsStringAsync().Result;
-                // 4. Check the response status codes, else throws the error message to the client
-                if (responseMessage.IsSuccessStatusCode)
-                {
-                    // 5. Deserialize the object
-                    string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
-                    usersDeserialize = JsonSerializer.Deserialize<IList<User>>(readAsStringAsync);
-                    foreach (var x in usersDeserialize)
-                    {
-                        Console.WriteLine(x.Role.Name);
-                    }
-                }
-                else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
-                {
-                    throw new Exception(serverMessage);
-                }
-                else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
-                {
-                    throw new Exception(serverMessage);
-                }
-            
             }
+            // 3. Catch the exception in case the Server is not running 
+            catch (HttpRequestException e)
+            {
+                throw new Exception("No connection could be made because the server is not responding");
+            }
+
+            string serverMessage = responseMessage.Content.ReadAsStringAsync().Result;
+            // 4. Check the response status codes, else throws the error message to the client
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                // 5. Deserialize the object
+                string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
+                usersDeserialize = JsonSerializer.Deserialize<IList<User>>(readAsStringAsync);
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
+            {
+                throw new Exception(serverMessage);
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
+            {
+                throw new Exception(serverMessage);
+            }
+
             return usersDeserialize;
             // List<User> users = new List<User>();
             // users.Add(new User("HEJ", "tyuiop", "fgjukil", "dfgnhjmk", "tgyhuj", new Role("teacher"), ""));
             // return users;
         }
-         
+
         public async Task<User> FindByUsername(string username)
         {
             User userDeserialize = null;
             HttpResponseMessage responseMessage;
-            string userSerialized = JsonSerializer.Serialize(username);
-            var content = new StringContent(userSerialized, Encoding.UTF8, "application/json");
-            // 1. Send POST request
+            // 1. Send GET request
             try
             {
                 responseMessage =
-                    await client.PostAsync(uri + "/user/getUserByUsername", content);
+                    await client.GetAsync($"{uri}/user/getUserByUsername/{username}");
                 // 2. Check if the resource was found, else throw exception to the client
                 if (responseMessage.StatusCode == HttpStatusCode.NotFound)
                 {
@@ -201,21 +193,22 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
             else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
             {
                 throw new Exception(serverMessage);
-            }   else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
             {
                 throw new Exception(serverMessage);
             }
-           
+
             // User user = new User("HEJ", "tyuiop", "fgjukil", "dfgnhjmk", "tgyhuj", new Role("teacher"),"@");
             return userDeserialize;
             // return user;
         }
 
-        public async Task DeleteUser(string username)
+        public async Task<User> DeleteUser(User user)
         {
             User userDeserialize = null;
             HttpResponseMessage responseMessage;
-            string userSerialized = JsonSerializer.Serialize(username);
+            string userSerialized = JsonSerializer.Serialize(user);
             var content = new StringContent(userSerialized, Encoding.UTF8, "application/json");
             // 1. Send POST request
             try
@@ -241,16 +234,24 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
                 // 5. Deserialize the object
                 string readAsStringAsync = await responseMessage.Content.ReadAsStringAsync();
                 userDeserialize = JsonSerializer.Deserialize<User>(readAsStringAsync);
-                Console.WriteLine(userDeserialize.Role.Name);
             }
             else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
             {
                 throw new Exception(serverMessage);
-            }  
+            } else if (responseMessage.StatusCode == HttpStatusCode.Conflict)
+            {
 
+                throw new Exception(serverMessage);
+            }else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
+            {
+
+                throw new Exception(serverMessage);
+            }
+
+            return userDeserialize;
         }
 
-        public async Task UpdateUser(User user)
+        public async Task<User> UpdateUser(User user)
         {
             User userDeserialize = null;
             HttpResponseMessage responseMessage;
@@ -285,9 +286,15 @@ namespace SEP3.PaperlessExam.Data.PaperlessExamSevice
             else if (responseMessage.StatusCode == HttpStatusCode.BadRequest)
             {
                 throw new Exception(serverMessage);
-            }   
-           
+            }  else if (responseMessage.StatusCode == HttpStatusCode.ServiceUnavailable)
+            {
+                throw new Exception(serverMessage);
+            }
+            else if (responseMessage.StatusCode == HttpStatusCode.Conflict)
+            {
+                throw new Exception(serverMessage);
+            }
+            return userDeserialize;
         }
     }
-
 }
